b318573ea77e8574ec5c0b273ca7c0fa2d308744
ImExoOdeex <odeex722@gmail.com>
2023-02-05 21:36:02 +0100

    Upgrade check api route

--- app/routes/api/check.tsx
+++ app/routes/api/check.tsx
@@ -1,13 +1,18 @@
-
 import type { ActionArgs } from "@remix-run/node"
 import { getClientIPAddress } from "remix-utils";
 import { db } from "~/components/utils/db.server";
+
+// This is typically POST request to this route. I use it for adding discord server checks. You can't make this request, cuz you dont have super duper 2048 bit access token hah
 export async function action({ request }: ActionArgs) {
 
     const body: any = JSON.parse(await request.text())
-    console.log("body:", body);
 
-    if (body.token !== process.env.SUPER_DUPER_API_ACCESS_TOKEN) return "Super Duper Token does not match the real 2048 bit Super Duper Token!"
+    if (body.token !== process.env.SUPER_DUPER_API_ACCESS_TOKEN) {
+        return new Response("Super Duper Token does not match the real 2048 bit Super Duper Token!", {
+            // not allowed status code
+            status: 405
+        })
+    }
 
     const IP = getClientIPAddress(request.headers)
 
@@ -25,6 +30,7 @@ export async function action({ request }: ActionArgs) {
     return null
 };
 
+// return 404, since without it, it will throw error
 export async function loader() {
     throw new Response(null, {
         status: 404
