635def0eb313c3454433f496340a4d490ba0a77c
ImExoOdeex <odeex722@gmail.com>
2023-02-06 02:06:10 +0100

    Submitting state on input

--- app/components/layout/Header/ServerSearch.tsx
+++ app/components/layout/Header/ServerSearch.tsx
@@ -1,7 +1,7 @@
-import { Box, Flex, Icon, Input, Kbd, useEventListener } from "@chakra-ui/react";
+import { Box, Flex, HStack, Icon, Input, Kbd, Spinner, Text, VStack, useEventListener } from "@chakra-ui/react";
 import { useFetcher, useLocation } from "@remix-run/react";
 import { AnimatePresence } from "framer-motion";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { BiSearchAlt } from "react-icons/bi";
 import { useActionKey } from "~/components/utils/func/useActionKey";
 import { motion } from 'framer-motion'
@@ -24,8 +24,10 @@ export default function ServerSearch() {
         }
     })
     const fetcher = useFetcher()
-    const state = fetcher.state
+    const submitting = fetcher.state !== "idle"
     const { pathname } = useLocation()
+
+    const [server, setServer] = useState<string>()
 
     return (
         <>
@@ -44,8 +46,8 @@ export default function ServerSearch() {
                             <Flex pos={'relative'}>
                                 <Input variant={"filled"} w='100%' display={"block"} rounded={'xl'} border={"2px"} px={4} py={1.5} pl={10} pr={14} fontWeight={"normal"}
                                     _focus={{ borderColor: "brand" }} borderColor={"alpha100"}
-                                    id="search" ref={inputRef} disabled={state !== "idle"}
-                                    name="server"
+                                    id="search" ref={inputRef} disabled={submitting} minLength={1}
+                                    name="server" onChange={(e) => setServer(e.currentTarget.value)} value={server}
                                     placeholder="Server address"
                                 />
                                 <Flex pos={"absolute"} insetY={"0"} left={0} alignItems={"center"} pl={4}>
@@ -56,6 +58,35 @@ export default function ServerSearch() {
                                         {actionKey} K
                                     </Kbd>
                                 </Flex>
+
+                                <AnimatePresence mode="wait">
+                                    {submitting &&
+                                        <motion.div
+                                            style={{ position: 'absolute', top: "0", right: "0", left: "0", bottom: "0" }}
+                                            transition={{ duration: .33, ease: [0.25, 0.1, 0.25, 1] }}
+                                            initial={{ opacity: 0, y: "-25%" }}
+                                            animate={{ opacity: 1, y: 0 }}
+                                            exit={{ opacity: 0, y: "-25%" }}
+                                        >
+                                            <Flex w={{ base: '105%', sm: "100%" }} h='100%' bg='bg' align={'center'} alignItems='center' justifyContent={'center'}>
+                                                <HStack>
+
+                                                    <VStack spacing={0}>
+                                                        <Text fontWeight={400} fontSize={'12px'}>
+                                                            Fetching {server}
+                                                        </Text>
+                                                        <Text fontSize={'10px'} opacity={.7}>
+                                                            This shouldn't take longer than 5 seconds
+                                                        </Text>
+                                                    </VStack>
+
+                                                    <Spinner size={'sm'} />
+                                                </HStack>
+                                            </Flex>
+                                        </motion.div>
+                                    }
+                                </AnimatePresence>
+
                             </Flex>
                         </fetcher.Form>
                     </motion.div>
