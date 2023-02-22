092a8528cee23f55d983d56399c5e0615f47dfb3
ImExoOdeex <odeex722@gmail.com>
2023-02-22 22:28:36 +0100

    (nearly) working infinite scroll on checks & remove margin inline start on `ol` in tos

--- app/components/layout/server/ChecksTable.tsx
+++ app/components/layout/server/ChecksTable.tsx
@@ -1,82 +1,198 @@
-import { CheckIcon, SmallCloseIcon } from "@chakra-ui/icons"
-import { TableContainer, Table, TableCaption, Thead, Tr, Th, Tbody, Td, HStack, Tfoot, Flex, Heading, Text, useColorModeValue, Badge } from "@chakra-ui/react"
-import { type SOURCE } from "@prisma/client"
+import { CheckIcon, SmallCloseIcon } from "@chakra-ui/icons";
+import {
+  TableContainer,
+  Table,
+  TableCaption,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  HStack,
+  Tfoot,
+  Flex,
+  Heading,
+  Text,
+  useColorModeValue,
+  Badge,
+} from "@chakra-ui/react";
+import { type SOURCE } from "@prisma/client";
+import { useFetcher } from "@remix-run/react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
-type Check = { id: number; server: string; source: SOURCE; online: boolean; players: number; checked_at: Date; }
+type Check = {
+  id: number;
+  server: string;
+  source: SOURCE;
+  online: boolean;
+  players: number;
+  checked_at: Date;
+};
 type Props = {
-    server: string, checks: Check[]
-}
+  server: string;
+  checks: Check[];
+};
 
 export default function ChecksTable({ server, checks }: Props) {
-    const borderColor = useColorModeValue('#2f2e32', '#2f2e32 !important')
+  const borderColor = useColorModeValue("#2f2e32", "#2f2e32 !important");
 
-    const discordBg = useColorModeValue("rgba(88, 101, 242, 0.16)", "rgba(88, 147, 242, 0.12)")
-    const discordColor = useColorModeValue("discord.100", "#6f9ce6")
-    const apiColor = useColorModeValue("blue.500", "blue.200")
+  const discordBg = useColorModeValue(
+    "rgba(88, 101, 242, 0.16)",
+    "rgba(88, 147, 242, 0.12)"
+  );
+  const discordColor = useColorModeValue("discord.100", "#6f9ce6");
+  const apiColor = useColorModeValue("blue.500", "blue.200");
 
-    return (
-        <>
-            {checks.length ?
-                <TableContainer w='100%'>
-                    <Table variant='simple' size={'sm'}>
-                        <TableCaption>Last status checks for {server}</TableCaption>
-                        <Thead>
-                            <Tr sx={{ "> *": { borderColor: borderColor } }}>
-                                <Th>Date</Th>
-                                <Th>Status</Th>
-                                <Th>Platform</Th>
-                                <Th isNumeric>Players</Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            {checks.map((c: Check) => {
-                                return (
-                                    <Tr key={c.id}
-                                        _hover={{ bg: 'alpha' }} transition="background .05s"
-                                        sx={{ "> *": { borderColor: borderColor } }}>
-                                        <Td>{new Date(c.checked_at).toLocaleString()}</Td>
-                                        <Td>{c.online ?
-                                            <HStack rounded={'md'} color='green' bg={'rgba(72, 187, 120, 0.1)'} w='min-content' px={3} py={1}>
-                                                <Text textTransform={'none'} fontWeight={600}>Online</Text>
-                                                <CheckIcon />
-                                            </HStack>
-                                            :
-                                            <HStack rounded={'md'} color='red' bg={'rgba(187, 72, 72, 0.1)'} w='min-content' px={3} py={1}>
-                                                <Text textTransform={'none'}>Offline</Text>
-                                                <SmallCloseIcon />
-                                            </HStack>
-                                        }</Td>
-                                        <Td>
-                                            <Badge rounded={"md"} fontWeight={700} px={3} py={1}
-                                                bg={c.source == "WEB" ? "alpha200" : c.source == "DISCORD" ? discordBg : "rgba(144, 206, 244, 0.16)"}
-                                                color={c.source == "WEB" ? "text" : c.source == "DISCORD" ? discordColor : apiColor}
-                                            >{c.source}</Badge>
-                                        </Td>
-                                        <Td isNumeric>{c.players}</Td>
-                                    </Tr>
-                                )
-                            })}
-                        </Tbody>
-                        <Tfoot>
-                            <Tr>
-                                <Th>Date</Th>
-                                <Th>Status</Th>
-                                <Th>Platform</Th>
-                                <Th isNumeric>Players</Th>
-                            </Tr>
-                        </Tfoot>
-                    </Table>
-                </TableContainer>
+  const checksFetcher = useFetcher();
+  const checksNew = checksFetcher.data;
+  const [checksState, setChecksState] = useState(checks)
+  
+  const loadNext = () => {
+    const c = checksFetcher.data ?  0: 20;
+    const query = `/api/checks/get?c=${c}&server=${server}`;
+    checksFetcher.load(query);
+  };
+  
+  const scrollListener = useRef(loadNext);
+  
+  const onScroll = () => {
+    const documentHeight = document.documentElement.scrollHeight;
+    const scrollDifference = Math.floor(window.innerHeight + window.scrollY);
+    const scrollEnded = documentHeight == scrollDifference;
 
-                :
+    if (scrollEnded && checksFetcher.state === "idle") {
+      scrollListener.current();
+    }
+  };
 
-                <Flex w="100%">
-                    <Heading fontSize={'md'} justifySelf='center' textAlign='center' color={'red'} mx='auto'>
-                        There were no checks
-                    </Heading>
-                </Flex>
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", onScroll);
+    }
 
-            }
-        </>
-    )
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
+  const [height, setHeight] = useState(null);
+
+  // Set height of the parent container whenever photos are loaded
+  const divHeight = useCallback(
+    (node: any) => {
+      if (node !== null) {
+        setHeight(node.getBoundingClientRect().height);
+      }
+    },
+    [checksState]
+  );
+
+  
+  return (
+    <>
+      {checks.length ? (
+        <TableContainer w="100%">
+          <Table variant="simple" size={"sm"}>
+            <TableCaption>Last status checks for {server}</TableCaption>
+            <Thead>
+              <Tr sx={{ "> *": { borderColor: borderColor } }}>
+                <Th>Date</Th>
+                <Th>Status</Th>
+                <Th>Platform</Th>
+                <Th isNumeric>Players</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {checks.map((c: Check) => {
+                return (
+                  <Tr
+                    key={c.id}
+                    _hover={{ bg: "alpha" }}
+                    transition="background .05s"
+                    sx={{ "> *": { borderColor: borderColor } }}
+                  >
+                    <Td>{new Date(c.checked_at).toLocaleString()}</Td>
+                    <Td>
+                      {c.online ? (
+                        <HStack
+                          rounded={"md"}
+                          color="green"
+                          bg={"rgba(72, 187, 120, 0.1)"}
+                          w="min-content"
+                          px={3}
+                          py={1}
+                        >
+                          <Text textTransform={"none"} fontWeight={600}>
+                            Online
+                          </Text>
+                          <CheckIcon />
+                        </HStack>
+                      ) : (
+                        <HStack
+                          rounded={"md"}
+                          color="red"
+                          bg={"rgba(187, 72, 72, 0.1)"}
+                          w="min-content"
+                          px={3}
+                          py={1}
+                        >
+                          <Text textTransform={"none"}>Offline</Text>
+                          <SmallCloseIcon />
+                        </HStack>
+                      )}
+                    </Td>
+                    <Td>
+                      <Badge
+                        rounded={"md"}
+                        fontWeight={700}
+                        px={3}
+                        py={1}
+                        bg={
+                          c.source == "WEB"
+                            ? "alpha200"
+                            : c.source == "DISCORD"
+                            ? discordBg
+                            : "rgba(144, 206, 244, 0.16)"
+                        }
+                        color={
+                          c.source == "WEB"
+                            ? "text"
+                            : c.source == "DISCORD"
+                            ? discordColor
+                            : apiColor
+                        }
+                      >
+                        {c.source}
+                      </Badge>
+                    </Td>
+                    <Td isNumeric>{c.players}</Td>
+                  </Tr>
+                );
+              })}
+            </Tbody>
+            <Tfoot>
+              <Tr>
+                <Th>Date</Th>
+                <Th>Status</Th>
+                <Th>Platform</Th>
+                <Th isNumeric>Players</Th>
+              </Tr>
+            </Tfoot>
+          </Table>
+        </TableContainer>
+      ) : (
+        <Flex w="100%">
+          <Heading
+            fontSize={"md"}
+            justifySelf="center"
+            textAlign="center"
+            color={"red"}
+            mx="auto"
+          >
+            There were no checks
+          </Heading>
+        </Flex>
+      )}
+    </>
+  );
 }
---
+++ app/routes/api/checks.get.ts
@@ -0,0 +1,36 @@
+import { type LoaderArgs, json } from "@remix-run/node";
+import { db } from "~/components/utils/db.server";
+
+export async function loader({ request }: LoaderArgs) {
+  const url = new URL(request.url);
+
+  const server = url.searchParams.get("server");
+  if (!server) {
+    return null;
+  }
+  const c = url.searchParams.get("c") || 0;
+
+  const checks = await db.check.findMany({
+    where: {
+      server: {
+        contains: server,
+      },
+      bedrock: false,
+    },
+    select: {
+      id: true,
+      server: false,
+      online: true,
+      players: true,
+      source: true,
+      checked_at: true,
+    },
+    orderBy: {
+      id: "desc",
+    },
+    take: 20,
+    skip: Number(c) ?? 0,
+  });
+
+  return json({ checks });
+}
